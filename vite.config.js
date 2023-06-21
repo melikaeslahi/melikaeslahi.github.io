ea5bac86758e15f89c384e605483533813cdd910
melikaeslahi <Melika.eslahi@gmail.com>
2023-06-21 19:40:17 +0430

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+ 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: path.join(__dirname, 'src'),
   plugins: [react()],
 })
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig(({ command }) => {
-//   const config = {
-//     plugins: [react()],
-//     base: '/',
-//   }
-
-//   if (command !== 'serve') {
-//     config.base = '/react-vite-gh-pages/'
-//   }
-
-//   return config
-// })
+ 
