8c1776780e2c125b5e211a8aa86a6c08bfe74e52
melikaeslahi <79050707+melikaeslahi@users.noreply.github.com>
2024-03-25 00:32:08 +0330

    imagechanges About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import { Helmet } from 'react-helmet';
-import  {profile} from '../assets/image';
+import  {profile} from '../assets/image/profile.jpg';
 import { about } from '../constants/about';
 import  Resume from '../assets/pdf/melikaeslahi.pdf'
  
